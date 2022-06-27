058e18bfa125b3ab8cc6e685708cbe063ab353f3
pranav <pranavpriyatosh@gmail.com>
2022-06-27 14:06:44 +0530

    created a get route using express

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const app = express()
 
+
+app.get("/", (req,resp) => { 
+resp.send("Api is running")
+
+})
+
 app.listen(5000,console.log('Server running on port 5000'))
