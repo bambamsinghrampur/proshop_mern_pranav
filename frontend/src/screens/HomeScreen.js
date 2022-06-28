be47b2bc65c029f19f436e0b45b5800709b5b51d
pranav <pranavpriyatosh@gmail.com>
2022-06-28 13:57:47 +0530

    redux integrated & working

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,41 +1,36 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
- 
+  const dispatch = useDispatch()
 
-    //not use static products json but make a api calls
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
-const [products,setProducts] = useState([])
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
 
-useEffect(() => {
-
-  const fetchProducts = async () => {
-
-    const {data} = await axios.get('/api/products')
-
-    setProducts(data)
-  }
-      
- fetchProducts()
-
-},[])
-
-return (
-    <Container>
-<Row>
-{products.map( product => (
-
-
-<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-  <Product product={product} />
-</Col> ))}
-</Row>
-
-    </Container>
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
   )
 }
 
